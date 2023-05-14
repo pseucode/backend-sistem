c4de04e3a46c7ecd3dc236fe9cc5dccc3b5e6fd5
pseucode <fathurenggaryansyah.gmail.com>
2023-05-14 19:44:56 +0700

    Homepage update ke-2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: '0.0.0.0',
+        host: 'localhost',
     });
 
 server.route(routes);

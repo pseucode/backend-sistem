031825787849de514a8afb448d6c24bc3ddc6ad7
pseucode <fathurenggaryansyah.gmail.com>
2023-06-01 16:02:45 +0700

    Update #3

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const routes = require('./routes');
 const Hapi = require('@hapi/hapi');
 const init = async () => {
     const server = Hapi.server({
-        port: 5000,
-        host: 'http://0.0.0.0',
+        port: 8080,
+        host: '0.0.0.0',
     });
 
 server.route(routes);
